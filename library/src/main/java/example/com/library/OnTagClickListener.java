2fa19dab77ab4f2ef5324debc52ba2636f42a9b2
datqtse02586 <datqtse02586@fpt.edu.vn>
2015-07-12 00:45:21 +0700

    add Tag library

---
+++ library/src/main/java/example/com/library/OnTagClickListener.java
@@ -0,0 +1,8 @@
+package example.com.library;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagClickListener {
+	void onTagClick(Tag tag, int position);
+}
---
+++ library/src/main/java/example/com/library/OnTagDeleteListener.java
@@ -0,0 +1,8 @@
+package example.com.library;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagDeleteListener {
+	void onTagDeleted(Tag tag, int position);
+}
