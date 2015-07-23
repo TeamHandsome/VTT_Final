183cf2386d3b77b5a808c2aa1bf282dd0957d41b
duongtqse02581 <duongtqse02581@fpt.edu.vn>
2015-07-24 02:51:49 +0700

    ^^

--- app/src/main/java/example/com/demoapp/activity/MysentencesFragment.java
+++ app/src/main/java/example/com/demoapp/activity/MysentencesFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,43 +14,59 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import example.com.demoapp.R;
+import example.com.demoapp.adapter.ViewPagerAdapter;
+import example.com.demoapp.tabs.SlidingTabLayout;
 import example.com.demoapp.utility.Consts;
 
-public class MysentencesFragment extends Fragment {
-    Context context;
-    public MysentencesFragment(){
-    }
+public class MysentencesFragment extends Fragment {private ViewPager pager;
+    private ViewPagerAdapter adapter;
+    private SlidingTabLayout tabs;
+    CharSequence Titles[] = {"??","??"};
+    int Numboftabs = 2;
+
+    public static int sentences_id = Consts.NOT_FOUND;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_mysentences, container, false);
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_mysentences, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+//        getActivity().setSupportActionBar(toolbar);
+        //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        adapter = new ViewPagerAdapter((getActivity()).getSupportFragmentManager(), Titles,
+                Numboftabs,Consts.MY_SENTENCE_LIST);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) view.findViewById(R.id.pagerMS);
+        pager.setAdapter(adapter);
+        pager.setTag(Consts.MY_SENTENCE_LIST);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) view.findViewById(R.id.tabMS);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+        pager.setCurrentItem(0);
 
 
-
-        setHasOptionsMenu(true);
         return view;
+
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        (getActivity()).getMenuInflater().inflate(R.menu.menu_mysentences, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
+    public void slidingTab() {
+        // // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                getActivity().onBackPressed();
-                return true;
-            case R.id.action_addNew:
-                Intent intent = new Intent(getActivity(), AddEditMySentencesActivity.class);
-                intent.putExtra(Consts.ACTION_TYPE,Consts.ADD_MY_SEN);
-                startActivity(intent);
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
