a5928f9d99bdf3a9e2e2e4893c158efddc171693
duongtqse02581 <duongtqse02581@fpt.edu.vn>
2015-07-24 04:06:16 +0700

    zz

---
+++ app/src/main/java/example/com/demoapp/adapter/HistorySentencesAdapter.java
@@ -0,0 +1,40 @@
+package example.com.demoapp.adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import example.com.demoapp.model.DAO.FavoriteDAO;
+import example.com.demoapp.model.SentenceItem;
+import example.com.demoapp.utility.Common;
+import example.com.demoapp.utility.Message;
+
+/**
+ * Created by dmonkey on 7/24/2015.
+ */
+public class HistorySentencesAdapter extends BaseSentencesAdapter {
+
+    public HistorySentencesAdapter(Activity context, int idLayoutResource, ArrayList<SentenceItem> listSentences) {
+        super(context, idLayoutResource, listSentences);
+        this.context = context;
+        this.idLayoutResource = idLayoutResource;
+        this.listSentences = listSentences;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = super.getView(position, convertView, parent);
+
+        this.setUpSound(holder, position);
+        this.setUpBtnDelete(holder,convertView,position);
+
+        return convertView;
+    }
+
+    @Override
+    protected void onclickDelete(int position) {
+    }
+}
+
