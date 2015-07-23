0db836c0bab6ff2ad0f000e2bc6d02abbbc356d8
duongtqse02581 <duongtqse02581@fpt.edu.vn>
2015-07-24 04:06:55 +0700

    cc

---
+++ app/src/main/java/example/com/demoapp/model/DAO/HistoryDAO.java
@@ -0,0 +1,48 @@
+package example.com.demoapp.model.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import example.com.demoapp.model.SentenceItem;
+import example.com.demoapp.utility.DbHelper;
+
+/**
+ * Created by dmonkey on 7/24/2015.
+ */
+public class HistoryDAO extends BaseDAO {
+    DbHelper mDbHelper;
+    public HistoryDAO(Context context) {
+        try {
+            mDbHelper = new DbHelper(context);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<SentenceItem> getAllHistory(){
+        ArrayList<SentenceItem> arrayList = null;
+        String query ="SELECT * FROM sentences, history WHERE sentences._ids = history.sentences_id";
+        this.rawQueryReadonly(query);
+
+        if(cursor.moveToFirst())
+        {
+            arrayList = new ArrayList<>();
+            do
+            {
+                SentenceItem item = new SentenceItem();
+                item.setId(cursor.getString((cursor.getColumnIndex(DbHelper.DB_SENTENCES_ID))));
+                item.setNameJp(cursor.getString(cursor.getColumnIndex(DbHelper.DB_SENTENCES_JP)));
+                item.setNameVn(cursor.getString(cursor.getColumnIndex(DbHelper.DB_SENTENCES_VN)));
+                item.setSound(cursor.getString(cursor.getColumnIndex(DbHelper.DB_SENTENCES_SOUND)));
+                if (cursor.getString(cursor.getColumnIndex(DbHelper.DB_SENTENCES_IMAGE))!=null){
+                    item.setImage(cursor.getString(cursor.getColumnIndex(DbHelper.DB_SENTENCES_IMAGE)));
+                }
+                arrayList.add(item);
+            }while(cursor.moveToNext());
+        }
+        close();
+        return arrayList;
+    }
+}
